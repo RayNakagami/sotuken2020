7a42591b5051188a69779dc47e78549880b593fc
RayN <ray.nakagami@gmail.com>
2020-12-07 18:54:42 +0900

    add script(move,event)

---
+++ Assets/Script/Event.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event : MonoBehaviour
+{
+    int score;
+
+    private Text scoreBord;
+
+    public GameObject textbox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.score = 0;
+        this.scoreBord = textbox.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ChangeScore()
+    {
+        score++;
+        scoreBord = "SCORE:" + score.ToString();
+    }
+}
