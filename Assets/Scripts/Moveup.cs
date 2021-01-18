a714db2533e777a408b236b58b5dca1d0bf3194c
RayN <ray.nakagami@gmail.com>
2021-01-18 19:18:17 +0900

    resolve conflict

--- Assets/Scripts/Moveup.cs
+++ Assets/Scripts/Moveup.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Moveup_semi : MonoBehaviour
+public class Moveup : MonoBehaviour
 {
     Vector3 pos;
     // Start is called before the first frame update
     void Start()
     {
         pos = this.gameObject.transform.position;
-
+        Debug.Log("A");
     }
 
     // Update is called once per frame
@@ -20,5 +20,7 @@ public class Moveup_semi : MonoBehaviour
         if (Input.GetKey(KeyCode.UpArrow)) pos.z += 0.1f;
         if (Input.GetKey(KeyCode.DownArrow)) pos.z -= 0.1f;
         this.gameObject.transform.position = this.pos;
+        this.gameObject.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        // Debug.Log(this.gameObject.transform.rotation);
     }
 }
---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public GameObject EventListner;
+    public GameObject semi;
+
+    bool isSemi;
+    // eventListnerScript script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isSemi = true;
+        semi.SetActive(true);
+        // Debug.Log("s");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionStay(Collision C)
+    {
+        if (C.gameObject.name == "SD_unitychan")
+        {
+            if (isSemi)
+            {
+                EventListner.GetComponent<Event>().AddScore();
+                isSemi = false;
+                semi.SetActive(false);
+                Debug.Log(semi.gameObject.name);
+            }
+        }
+        // Debug.Log(C.gameObject.name);
+        // Debug.Log(semi.gameObject.name);
+    }
+
+    public void SetSemi()
+    {
+        if (!isSemi)
+        {
+            isSemi = true;
+            semi.SetActive(true);
+        }
+    }
+
+}
